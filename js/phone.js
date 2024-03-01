3e46715956fabc2d9c2d88573851e597becf8ac5
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-03-01 22:38:52 +0600

    modal section also complete and finally all done

--- js/phone.js
+++ js/phone.js
@@ -38,7 +38,7 @@ const displayPhones = (phones, isShowAll) => {
            <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-center">
-                <button class="btn btn-primary">SHOW DETAILS</button>
+                <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">SHOW DETAILS</button>
             </div>
         </div>
         `;
@@ -51,6 +51,38 @@ const displayPhones = (phones, isShowAll) => {
         toggleLoading(false);
     })
 }
+
+
+// show details
+const handleShowDetail = async (id) => {
+    console.log(id);
+
+    // load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const phone = await res.json();
+    // console.log(data);
+
+
+    showPhoneDetails(phone);
+}
+
+// show phone details
+function showPhoneDetails(phone) {
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.data.name;
+
+    const phoneDetail = document.getElementById('show-phone-detail');
+    phoneDetail.innerHTML = `
+        <img src="${phone.data.image}"> <br>
+        <p><b>Chipset:</b> ${phone.data.mainFeatures.chipSet}</p>
+        <p><b>displaySize:</b> ${phone.data.mainFeatures.displaySize}</p>
+        <p><b>memory:</b> ${phone.data.mainFeatures.memory}</p>
+        <p><b>storage:</b> ${phone.data.mainFeatures? phone.data.mainFeatures.storage : 'storage details not aviable'}</p>
+    `
+    my_modal_3.showModal();
+}
+
 
 
 // handle search
